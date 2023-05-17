3e94750aaf2cec6219d42afc9b6f72713a4343f4
David Thompson <david.thompson@familysearch.org>
2023-05-17 12:45:18 -0600

    Found a new rich text editor that works

--- app/routes/admin.new.jsx
+++ app/routes/admin.new.jsx
@@ -1,15 +1,13 @@
 // https://ckeditor.com/docs/ckeditor5/latest/installation/getting-started/getting-and-setting-data.html
 // Docs for WYSIWYG editor
 
-import Editor from "./components/Editor";
+import EditorSlate from "./components/Editor";
 
 
 export default function AdminNew() {
     return (
-        // TODO: Well, this editor is kinda broken... first trying turning into component, but may need to find a different plugin
-        // <Editor />
         <div>
-            <Editor />
+            <EditorSlate />
         </div>
     )
 }
--- app/routes/components/Editor.jsx
+++ app/routes/components/Editor.jsx
@@ -1,80 +1,100 @@
-import {$getRoot, $getSelection} from 'lexical';
-import {useEffect} from 'react';
+import { useCallback, useEffect, useMemo, useState } from "react";
+import { createEditor } from "slate";
+import { Editable, Slate, withReact } from "slate-react";
+import CustomEditor from "./EditorHelpers";
 
-import {LexicalComposer} from '@lexical/react/LexicalComposer';
-import {RichTextPlugin} from '@lexical/react/LexicalRichTextPlugin'
-import {ContentEditable} from '@lexical/react/LexicalContentEditable';
-import {HistoryPlugin} from '@lexical/react/LexicalHistoryPlugin';
-import {OnChangePlugin} from '@lexical/react/LexicalOnChangePlugin';
-import {LinkPlugin} from '@lexical/react/LexicalLinkPlugin';
-import {ListPlugin} from '@lexical/react/LexicalListPlugin';
-import {TabIndentationPlugin} from '@lexical/react/LexicalTabIndentationPlugin';
+const initialValue = [
+  {
+    type: "paragraph",
+    children: [{ text: "A line of text in a paragraph." }],
+  },
+];
 
-import {useLexicalComposerContext} from '@lexical/react/LexicalComposerContext';
-import LexicalErrorBoundary from '@lexical/react/LexicalErrorBoundary';
+function EditorSlate() {
+  const [editor] = useState(() => withReact(createEditor()));
 
-import theme from '../../styles/lexical/PlaygroundEditorTheme';
+  const renderElement = useCallback((props) => {
+    switch (props.element.type) {
+      case "code":
+        return <CodeElement {...props} />;
+      default:
+        return <DefaultElement {...props} />;
+    }
+  }, []);
 
-// TODO: So turns out this Lexical Editor is mega complicated... 
-// Maybe try to find something somewhat more simple...
-
-// When the editor changes, you can get notified via the
-// LexicalOnChangePlugin!
-function onChange(editorState) {
-  editorState.read(() => {
-    // Read the contents of the EditorState here.
-    const root = $getRoot();
-    const selection = $getSelection();
-
-    console.log(root, selection);
-  });
-}
-
-// Lexical React plugins are React components, which makes them
-// highly composable. Furthermore, you can lazy load plugins if
-// desired, so you don't pay the cost for plugins until you
-// actually use them.
-function MyCustomAutoFocusPlugin() {
-  const [editor] = useLexicalComposerContext();
-
-  useEffect(() => {
-    // Focus the editor when the effect fires!
-    editor.focus();
-  }, [editor]);
-
-  return null;
-}
-
-// Catch any errors that occur during Lexical updates and log them
-// or throw them as needed. If you don't throw them, Lexical will
-// try to recover gracefully without losing user data.
-function onError(error) {
-  console.error(error);
-}
-
-function Editor() {
-  const initialConfig = {
-    namespace: 'MyEditor', 
-    theme,
-    onError,
-  };
+  const renderLeaf = useCallback((props) => {
+    return <Leaf {...props} />;
+  }, []);
 
   return (
-    <LexicalComposer initialConfig={initialConfig}>
-      <OnChangePlugin onChange={onChange} />
-      <HistoryPlugin />
-      <MyCustomAutoFocusPlugin />
-      <RichTextPlugin
-        contentEditable={<ContentEditable />}
-        placeholder={<div>Enter some text...</div>}
-        ErrorBoundary={LexicalErrorBoundary}
+    <Slate
+      editor={editor}
+      value={initialValue}
+      onChange={(value) => {
+        const isAstChange = editor.operations.some(
+          (op) => "set_selection" !== op.type
+        );
+        if (isAstChange) {
+          const content = JSON.stringify(value);
+        }
+      }}
+    >
+      <div>
+        <button
+          onMouseDown={(event) => {
+            event.preventDefault();
+            CustomEditor.toggleBoldMark(editor);
+          }}
+        >
+          Bold
+        </button>
+      </div>
+      <Editable
+        renderElement={renderElement}
+        renderLeaf={renderLeaf}
+        onKeyDown={(event) => {
+          if (!event.ctrlKey) {
+            return;
+          }
+          switch (event.key) {
+            case "`": {
+              event.preventDefault();
+              CustomEditor.toggleCodeBlock();
+              break;
+            }
+            case "b": {
+              event.preventDefault();
+              CustomEditor.toggleBoldMark();
+              break;
+            }
+          }
+        }}
       />
-      <LinkPlugin />
-      <ListPlugin />
-      <TabIndentationPlugin />
-      
-
-    </LexicalComposer>
+    </Slate>
   );
 }
-export default Editor;
+
+const CodeElement = (props) => {
+  return (
+    <pre {...props.attributes}>
+      <code>{props.children}</code>
+    </pre>
+  );
+};
+
+const DefaultElement = (props) => {
+  return <p {...props.attributes}>{props.children}</p>;
+};
+
+const Leaf = (props) => {
+  return (
+    <span
+      {...props.attributes}
+      style={{ fontFamily: props.leaf.bold ? "lato-bold" : "lato" }}
+    >
+      {props.children}
+    </span>
+  );
+};
+
+export default EditorSlate;
