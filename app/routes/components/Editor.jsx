52d40783078d6811a77af3fbcdbb9147f2ee765c
David Thompson <david.thompson@familysearch.org>
2023-05-30 13:17:43 -0600

    Changed editor to Slate.js
    Lexical and all the other ones I tried were not rendering
    Slate seems intuitive for most things and is highly customizable.

--- app/routes/components/Editor.jsx
+++ app/routes/components/Editor.jsx
@@ -1,7 +1,21 @@
 import { useCallback, useEffect, useMemo, useState } from "react";
 import { createEditor } from "slate";
-import { Editable, Slate, withReact } from "slate-react";
-import CustomEditor from "./EditorHelpers";
+import { Editable, Slate, useSlate, withReact } from "slate-react";
+import * as Helper from "./EditorHelpers";
+import Button from "./SlateComponents/Button";
+import Icon from "./SlateComponents/Icon";
+import Toolbar from "./SlateComponents/ToolBar";
+import { withHistory } from "slate-history";
+import isHotkey from "is-hotkey";
+
+const HOTKEYS = {
+  "mod+b": "bold",
+  "mod+i": "italic",
+  "mod+u": "underline",
+  "mod+`": "code",
+};
+
+const TEXT_ALIGN_TYPES = ["left", "center", "right", "justify"];
 
 const initialValue = [
   {
@@ -11,61 +25,43 @@ const initialValue = [
 ];
 
 function EditorSlate() {
-  const [editor] = useState(() => withReact(createEditor()));
+  const editor = useMemo(() => withHistory(withReact(createEditor())), []);
+  let [editorContent, setEditorContent] = useState(0);
 
-  const renderElement = useCallback((props) => {
-    switch (props.element.type) {
-      case "code":
-        return <CodeElement {...props} />;
-      default:
-        return <DefaultElement {...props} />;
-    }
-  }, []);
+  useEffect(() => {
+    setLocalStorage(editorContent);
+  }, [editorContent]);
 
-  const renderLeaf = useCallback((props) => {
-    return <Leaf {...props} />;
-  }, []);
+  const renderElement = useCallback((props) => <Element {...props} />, []);
+  const renderLeaf = useCallback((props) => <Leaf {...props} />, []);
 
   return (
-    <Slate
-      editor={editor}
-      value={initialValue}
-      onChange={(value) => {
-        const isAstChange = editor.operations.some(
-          (op) => "set_selection" !== op.type
-        );
-        if (isAstChange) {
-          const content = JSON.stringify(value);
-        }
-      }}
-    >
-      <div>
-        <button
-          onMouseDown={(event) => {
-            event.preventDefault();
-            CustomEditor.toggleBoldMark(editor);
-          }}
-        >
-          Bold
-        </button>
-      </div>
+    <Slate editor={editor} value={initialValue}>
+      <Toolbar className="toolbar">
+        <MarkButton format="bold" icon="format_bold" />
+        <MarkButton format="italic" icon="format_italic" />
+        <MarkButton format="underline" icon="format_underlined" />
+        <MarkButton format="code" icon="code" />
+        <BlockButton format="heading-one" icon="looks_one" />
+        <BlockButton format="heading-two" icon="looks_two" />
+        <BlockButton format="block-quote" icon="format_quote" />
+        <BlockButton format="numbered-list" icon="format_list_numbered" />
+        <BlockButton format="bulleted-list" icon="format_list_bulleted" />
+        <BlockButton format="left" icon="format_align_left" />
+        <BlockButton format="center" icon="format_align_center" />
+        <BlockButton format="right" icon="format_align_right" />
+        <BlockButton format="justify" icon="format_align_justify" />
+      </Toolbar>
       <Editable
         renderElement={renderElement}
         renderLeaf={renderLeaf}
+        spellCheck
         onKeyDown={(event) => {
-          if (!event.ctrlKey) {
-            return;
-          }
-          switch (event.key) {
-            case "`": {
+          for (const hotkey in HOTKEYS) {
+            if (isHotkey(hotkey, event)) {
               event.preventDefault();
-              CustomEditor.toggleCodeBlock();
-              break;
-            }
-            case "b": {
-              event.preventDefault();
-              CustomEditor.toggleBoldMark();
-              break;
+              const mark = HOTKEYS[hotkey];
+              Helper.toggleMark(editor, mark);
             }
           }
         }}
@@ -74,27 +70,110 @@ function EditorSlate() {
   );
 }
 
-const CodeElement = (props) => {
-  return (
-    <pre {...props.attributes}>
-      <code>{props.children}</code>
-    </pre>
-  );
+const Element = ({ attributes, children, element }) => {
+  const style = { textAlign: element.align };
+  switch (element.type) {
+    case "block-quote":
+      return (
+        <blockquote style={style} {...attributes}>
+          {children}
+        </blockquote>
+      );
+    case "bulleted-list":
+      return (
+        <ul style={style} {...attributes}>
+          {children}
+        </ul>
+      );
+    case "heading-one":
+      return (
+        <h1 style={style} {...attributes}>
+          {children}
+        </h1>
+      );
+    case "heading-two":
+      return (
+        <h2 style={style} {...attributes}>
+          {children}
+        </h2>
+      );
+    case "list-item":
+      return (
+        <li style={style} {...attributes}>
+          {children}
+        </li>
+      );
+    case "numbered-list":
+      return (
+        <ol style={style} {...attributes}>
+          {children}
+        </ol>
+      );
+    default:
+      return (
+        <p style={style} {...attributes}>
+          {children}
+        </p>
+      );
+  }
 };
 
-const DefaultElement = (props) => {
-  return <p {...props.attributes}>{props.children}</p>;
+const Leaf = ({ attributes, children, leaf }) => {
+  if (leaf.bold) {
+    children = <strong>{children}</strong>;
+  }
+
+  if (leaf.code) {
+    children = <code>{children}</code>;
+  }
+
+  if (leaf.italic) {
+    children = <em>{children}</em>;
+  }
+
+  if (leaf.underline) {
+    children = <u>{children}</u>;
+  }
+
+  return <span {...attributes}>{children}</span>;
 };
 
-const Leaf = (props) => {
+const BlockButton = ({ format, icon }) => {
+  const editor = useSlate();
   return (
-    <span
-      {...props.attributes}
-      style={{ fontFamily: props.leaf.bold ? "lato-bold" : "lato" }}
+    <Button
+      active={Helper.isBlockActive(
+        editor,
+        format,
+        TEXT_ALIGN_TYPES.includes(format) ? "align" : "type"
+      )}
+      onMouseDown={(event) => {
+        event.preventDefault();
+        Helper.toggleBlock(editor, format);
+      }}
     >
-      {props.children}
-    </span>
+      <Icon>{icon}</Icon>
+    </Button>
   );
 };
+
+const MarkButton = ({ format, icon }) => {
+  const editor = useSlate();
+  return (
+    <Button
+      active={Helper.isMarkActive(editor, format)}
+      onMouseDown={(event) => {
+        event.preventDefault();
+        Helper.toggleMark(editor, format);
+      }}
+    >
+      <Icon>{icon}</Icon>
+    </Button>
+  );
+};
+
+function setLocalStorage(content) {
+  localStorage.setItem("content", content);
+}
 
 export default EditorSlate;
