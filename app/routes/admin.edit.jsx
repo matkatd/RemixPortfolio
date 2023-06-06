c1184b621223fc427bb6d7629aa7add4d8d461e1
David Thompson <david.thompson@familysearch.org>
2023-06-06 11:02:15 -0600

    adding form for post submission
    Working on figuring out how to pass data from Tiptap into the action

--- app/routes/admin.edit.jsx
+++
@@ -1,5 +0,0 @@
-import Tiptap from "./components/Tiptap";
-
-export default function AdminEdit() {
-  return <Tiptap />;
-}
--- app/routes/components/CardList.jsx
+++ app/routes/components/CardList.jsx
@@ -1,11 +1,16 @@
 import ProjectCard from "~/routes/components/ProjectCard.jsx";
+import AdminCard from "./AdminCard";
 
-function CardList({ data }) {
+function CardList({ data, admin }) {
   return (
     <div id="card-grid">
-      {data?.map((project) => (
-        <ProjectCard project={project} key={project?.slug} />
-      ))}
+      {data?.map((project) => {
+        return !admin ? (
+          <ProjectCard project={project} key={project?.slug} />
+        ) : (
+          <AdminCard project={project} key={project?.slug} />
+        );
+      })}
     </div>
   );
 }
